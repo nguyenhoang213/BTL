0bfa37c820119849ee1614da25cf0c1e89eaf525
Dinh <vandinh51122@gmail.com>
2024-10-13 12:01:44 +0700

    Dinhtest

--- header.php
+++ header.php
@@ -6,32 +6,36 @@
 <!-- Header -->
 <div class="header-wrapper">
     <div class="box-header">
-        <a href="/BTL/index.php">HOME</a>
-        <form action="./find_product.php" method="GET" style="width:30vw">
+        <div class="box-header-logo">
+            <a href="/BTL/index.php">
+                <img style="font-size: 10px;" src="/BTL/src/assets/images/logo-01.png" alt="">
+            </a>
+        </div>
+        <form action="./find_product.php" method="GET" style="width:450px">
             <div class="share-header">
                 <input name="keyword" type="text" placeholder="Gõ từ khóa tìm kiếm..." />
                 <button style="border:none; background-color: inherit"><i
                         class="fa-solid fa-magnifying-glass"></i></button>
             </div>
         </form>
-        <div class="btn-header">
-            <div class="btn-hotline">
-                <button class="btn-phone">
-                    <i class="fa-solid fa-phone"></i>
-                </button>
-                <button class="btn-number"> Mua hàng Online <br />
-                    <b>1900.636.648</b>
-                </button>
-                <!-- Nút kích hoạt Modal -->
-                <?php
-                session_start();
-                if (isset($_SESSION["user_id"])) {
-                    $user_id = $_SESSION["user_id"];
-                    $sql_user = "SELECT * FROM user WHERE user_id = '$user_id'";
-                    $result_user = $conn->query($sql_user);
-                    if ($result_user->num_rows > 0) {
-                        while ($row_user = $result_user->fetch_assoc()) {
-                            echo '
+
+        <div class="btn-hotline">
+            <button class="btn-phone">
+                <i class="fa-solid fa-phone"></i>
+            </button>
+            <button class="btn-number"> Mua hàng Online <br />
+                <b>1900.636.648</b>
+            </button>
+            <!-- Nút kích hoạt Modal -->
+            <?php
+            session_start();
+            if (isset($_SESSION["user_id"])) {
+                $user_id = $_SESSION["user_id"];
+                $sql_user = "SELECT * FROM user WHERE user_id = '$user_id'";
+                $result_user = $conn->query($sql_user);
+                if ($result_user->num_rows > 0) {
+                    while ($row_user = $result_user->fetch_assoc()) {
+                        echo '
                             <div class="user-dropdown">
                                 <button class="user-button">Xin chào, ' . $row_user['first_name'] . ' ' . $row_user['last_name'] . '</button>
                                 <div class="dropdown-content">
@@ -40,23 +44,23 @@
                                     <a href="/BTL/login/logout.php">Đăng xuất</a>
                                 </div>
                             </div>';
-                        }
                     }
-                } else {
-                    ?>
-                    <button class="button-custom" type="button" class="btn btn-primary" data-bs-toggle="modal"
-                        data-bs-target="#authModal" onclick="window.location.href='/BTL/login/login.php'">
-                        Đăng Ký/Đăng Nhập <i class="fa-solid fa-user"></i>
-                    </button>
-                    <?php
                 }
+            } else {
                 ?>
-                <button class="btn-shopping">
-                    <a class="nav-link" href="/BTL/cart/cart.php"><i class="fas fa-shopping-cart"></i><span
-                            id="cart-item" class="badge badge-danger"></span></a>
+                <button class="button-custom" type="button" class="btn btn-primary" data-bs-toggle="modal"
+                    data-bs-target="#authModal" onclick="window.location.href='/BTL/login/login.php'">
+                    Đăng Ký/Đăng Nhập <i class="fa-solid fa-user"></i>
                 </button>
-            </div>
+                <?php
+            }
+            ?>
+            <button class="btn-shopping">
+                <a class="nav-link" href="/BTL/cart/cart.php"><i class="fas fa-shopping-cart"></i><span id="cart-item"
+                        class="badge badge-danger"></span></a>
+            </button>
         </div>
+
     </div>
 </div>
 <!-- end header -->
