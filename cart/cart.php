53d44801c369c49ab0f1036d6c542f0720a62574
Dinh <vandinh51122@gmail.com>
2024-10-13 20:10:24 +0700

    updategiaodien3

--- cart/cart.php
+++ cart/cart.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Giỏ hàng</title>
     <link rel="stylesheet" href="/BTL/css/index.css" />
+    <link rel="stylesheet" href="/BTL/css/footer.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
@@ -245,6 +246,9 @@ input[type="number"] {
             </div>
         </div>
     </div>
+    <?php
+    include("../footer.php")
+    ?>
     <script src="https://kit.fontawesome.com/0236bf0649.js" crossorigin="anonymous"></script>
     <script src='https://cdnjs.cloudflare.com/connection/libs/jquery/3.5.1/jquery.min.js'></script>
     <script src='https://cdnjs.cloudflare.com/connection/libs/twitter-bootstrap/4.5.2/js/bootstrap.min.js'></script>
