51c599beafa0848d4c765cae6434fc9933174b0b
nguyenhoang213 <124745303+nguyenhoang213@users.noreply.github.com>
2024-10-14 22:26:34 +0700

    update

--- find_product.php
+++ find_product.php
@@ -1,36 +1,36 @@
 <?php
-    include("./connection.php");
-    $keyword = isset($_GET['keyword']) ? $_GET['keyword'] : '';
-    $category = isset($_GET['category']) ? $_GET['category'] : 'Tất cả';
-    $price_range = isset($_GET['price-range']) ? $_GET['price-range'] : 10000000;
-    $rating = isset($_GET['rating']) ? $_GET['rating'] : 'Tất cả';
-    $sort = isset($_GET['sort']) ? $_GET['sort'] : '';
-    
-    $sql = "SELECT * FROM product WHERE product_name like '$keyword'";
+include("./connection.php");
+$keyword = isset($_GET['keyword']) ? $_GET['keyword'] : '';
+$category = isset($_GET['category']) ? $_GET['category'] : 'Tất cả';
+$price_range = isset($_GET['price-range']) ? $_GET['price-range'] : 10000000;
+$rating = isset($_GET['rating']) ? $_GET['rating'] : 'Tất cả';
+$sort = isset($_GET['sort']) ? $_GET['sort'] : '';
 
-    if ($category !== 'Tất cả') {
-        $sql .= " AND product_id IN (SELECT product_id FROM product_category WHERE category_id in (SELECT category_id FROM category WHERE category_name like '%$category%'))";
-    }
+$sql = "SELECT * FROM product WHERE product_name like '%$keyword%'";
 
-    if (!empty($price_range)) {
-        $sql .= " AND price <= $price_range";
-    }
+if ($category !== 'Tất cả') {
+    $sql .= " AND product_id IN (SELECT product_id FROM product_category WHERE category_id in (SELECT category_id FROM category WHERE category_name like '%$category%'))";
+}
 
-    if ($rating !== 'Tất cả') {
-        $sql .= " AND rating >= " . (int)$rating;
-    }
+if (!$price_range) {
+    $sql .= " AND price <= $price_range";
+}
 
-     if (!empty($sort)) {
-        if ($sort === 'price-asc') {
-            $sql .= " ORDER BY price ASC"; // Giá tăng dần
-        } elseif ($sort === 'price-desc') {
-            $sql .= " ORDER BY price DESC"; // Giá giảm dần
-        } elseif ($sort === 'name-asc') {
-            $sql .= " ORDER BY name ASC"; // Tên sản phẩm từ A đến Z
-        } elseif ($sort === 'name-desc') {
-            $sql .= " ORDER BY name DESC"; // Tên sản phẩm từ Z đến A
-        }
+if ($rating !== 'Tất cả') {
+    $sql .= " AND rating >= " . (int) $rating;
+}
+
+if (!empty($sort)) {
+    if ($sort === 'price-asc') {
+        $sql .= " ORDER BY price ASC"; // Giá tăng dần
+    } elseif ($sort === 'price-desc') {
+        $sql .= " ORDER BY price DESC"; // Giá giảm dần
+    } elseif ($sort === 'name-asc') {
+        $sql .= " ORDER BY name ASC"; // Tên sản phẩm từ A đến Z
+    } elseif ($sort === 'name-desc') {
+        $sql .= " ORDER BY name DESC"; // Tên sản phẩm từ Z đến A
     }
+}
 ?>
 
 <!DOCTYPE html>
@@ -46,7 +46,7 @@
 
 <body>
     <?php
-        include("header.php");
+    include("header.php");
     ?>
     <!-- end header -->
 
@@ -196,11 +196,11 @@
     <h1 style="margin: 10px 0 10px 200px">Kết quả tìm kiếm</h1>
 
     <script>
-    function price_range_display() {
-        var price = document.getElementById('price-range').value;
-        document.getElementById('price_display').textContent = parseInt(price).toLocaleString('vi-VN') + " VNĐ";
+        function price_range_display() {
+            var price = document.getElementById('price-range').value;
+            document.getElementById('price_display').textContent = parseInt(price).toLocaleString('vi-VN') + " VNĐ";
 
-    }
+        }
     </script>
 
     <form action="" method="GET">
@@ -209,14 +209,22 @@
                 <input type="hidden" name="keyword" value="<?php echo $keyword ?>">
                 <label for="category">Danh mục</label>
                 <select name="category">
-                    <option <?php if ($category == 'Tất cả') echo 'selected'; ?>>Tất cả</option>
-                    <option <?php if ($category == 'Điện thoại') echo 'selected'; ?>>Điện thoại</option>
-                    <option <?php if ($category == 'Laptop') echo 'selected'; ?>>Laptop</option>
-                    <option <?php if ($category == 'Table') echo 'selected'; ?>>Table</option>
-                    <option <?php if ($category == 'Tai nghe') echo 'selected'; ?>>Tai nghe</option>
-                    <option <?php if ($category == 'Chuột máy tính') echo 'selected'; ?>>Chuột máy tính</option>
-                    <option <?php if ($category == 'PC') echo 'selected'; ?>>PC</option>
-                    <option <?php if ($category == 'Dây sạc') echo 'selected'; ?>>Dây sạc</option>
+                    <option <?php if ($category == 'Tất cả')
+                        echo 'selected'; ?>>Tất cả</option>
+                    <option <?php if ($category == 'Điện thoại')
+                        echo 'selected'; ?>>Điện thoại</option>
+                    <option <?php if ($category == 'Laptop')
+                        echo 'selected'; ?>>Laptop</option>
+                    <option <?php if ($category == 'Table')
+                        echo 'selected'; ?>>Table</option>
+                    <option <?php if ($category == 'Tai nghe')
+                        echo 'selected'; ?>>Tai nghe</option>
+                    <option <?php if ($category == 'Chuột máy tính')
+                        echo 'selected'; ?>>Chuột máy tính</option>
+                    <option <?php if ($category == 'PC')
+                        echo 'selected'; ?>>PC</option>
+                    <option <?php if ($category == 'Dây sạc')
+                        echo 'selected'; ?>>Dây sạc</option>
                 </select>
             </div>
 
@@ -231,10 +239,14 @@
             <div class="filter-item">
                 <label>Đánh giá</label>
                 <select>
-                    <option <?php if ($rating == 'Tất cả') echo 'selected'; ?>>Tất cả</option>
-                    <option value="4" <?php if ($rating == '4') echo 'selected'; ?>>4 sao trở lên</option>
-                    <option value="3" <?php if ($rating == '3') echo 'selected'; ?>>3 sao trở lên</option>
-                    <option value="2" <?php if ($rating == '2') echo 'selected'; ?>>2 sao trở lên</option>
+                    <option <?php if ($rating == 'Tất cả')
+                        echo 'selected'; ?>>Tất cả</option>
+                    <option value="4" <?php if ($rating == '4')
+                        echo 'selected'; ?>>4 sao trở lên</option>
+                    <option value="3" <?php if ($rating == '3')
+                        echo 'selected'; ?>>3 sao trở lên</option>
+                    <option value="2" <?php if ($rating == '2')
+                        echo 'selected'; ?>>2 sao trở lên</option>
                 </select>
             </div>
 
@@ -242,11 +254,15 @@
                 <label for="sort">Sắp xếp theo</label>
                 <select name="sort">
                     <option value="">Mặc định</option>
-                    <option value="price-asc" <?php if ($sort == 'price-asc') echo 'selected'; ?>>Giá tăng dần</option>
-                    <option value="price-desc" <?php if ($sort == 'price-desc') echo 'selected'; ?>>Giá giảm dần
+                    <option value="price-asc" <?php if ($sort == 'price-asc')
+                        echo 'selected'; ?>>Giá tăng dần</option>
+                    <option value="price-desc" <?php if ($sort == 'price-desc')
+                        echo 'selected'; ?>>Giá giảm dần
                     </option>
-                    <option value="name-asc" <?php if ($sort == 'name-asc') echo 'selected'; ?>>Tên A-Z</option>
-                    <option value="name-desc" <?php if ($sort == 'name-desc') echo 'selected'; ?>>Tên Z-A</option>
+                    <option value="name-asc" <?php if ($sort == 'name-asc')
+                        echo 'selected'; ?>>Tên A-Z</option>
+                    <option value="name-desc" <?php if ($sort == 'name-desc')
+                        echo 'selected'; ?>>Tên Z-A</option>
                 </select>
             </div>
             <button class="filter-button">lọc</button>
@@ -256,34 +272,34 @@
         <div id="message"></div>
         <div class="row mt-2 pb-3">
             <?php
-                $result = $conn->query($sql);
-                if($result->num_rows > 0) {
-                    while ($row = $result->fetch_assoc()) {
-                        echo ' <div class="col-sm-6 col-md-4 col-lg-3 mb-2">
+            $result = $conn->query($sql);
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    echo ' <div class="col-sm-6 col-md-4 col-lg-3 mb-2">
                         <div class="list-product">
                             <a href="./product/product_detail.php?id=' . $row['product_id'] . '">
                             <div>
-                                <img src="/BTL/src/assets/uploads/product/'. $row['image'] . '" class="card-img-top" height="250">
+                                <img src="/BTL/src/assets/uploads/product/' . $row['image'] . '" class="card-img-top" height="250">
                                 <div class="card-body p-1">
-                                    <h4 class="card-title text-center text-info"> '. $row['product_name'] . ' </h4>
-                                    <h5 class="card-text text-center text-danger"> '. number_format($row['price']) .' VNĐ</h5>
+                                    <h4 class="card-title text-center text-info"> ' . $row['product_name'] . ' </h4>
+                                    <h5 class="card-text text-center text-danger"> ' . number_format($row['price']) . ' VNĐ</h5>
                                 </div>
                             </a>
                             </div>
                         </div>
                     </div>';
-                    }
-                } else {
-                    echo 'Không có sản phẩm nào';
                 }
-                    ?>
+            } else {
+                echo 'Không có sản phẩm nào';
+            }
+            ?>
         </div>
     </div>
 
     <script src="https://kit.fontawesome.com/0236bf0649.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
+        </script>
 </body>
 
 </html>
--- login/login.php
+++ login/login.php
@@ -1,6 +1,7 @@
 <?php
-include("../connection.php");
 session_start();
+
+include("../connection.php");
 if (isset($_SESSION["user_id"])) {
     echo "<script>window.location.href='http://localhost/BTL/';</script>";
 }
